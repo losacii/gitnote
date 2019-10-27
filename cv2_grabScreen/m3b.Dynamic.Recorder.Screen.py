786a9503892cfd700b7bcc335fdb07af3eff4ff4
apple <apple@appledeMac-mini.local>
2019-10-27 14:50:17 +0800

    "update"

--- cv2_grabScreen/m3b.Dynamic.Recorder.Screen.py
+++ cv2_grabScreen/m3b.Dynamic.Recorder.Screen.py
@@ -2,25 +2,33 @@ import time
 import cv2 as cv
 import mss
 import numpy as np
-from PIL import ImageGrab
-import sys
 
 '''
 1. R 作为录像开关（切换器）
 2. 屏幕没有变化，则暂停录像，屏幕有变动则录像
 '''
 
-put = sys.stdout.write
-
 WIDTH = 1280; HEIGHT = 720
 monitor = {"top":45, "left":0, "width":WIDTH, "height":HEIGHT}
 sct = mss.mss()
 cv.namedWindow("Video Stream Monitor")
 
-# Recorder-1: Define the codec and create VideoWriter object
-gettime = time.strftime("%Y%m%d_%H%M%S")
-fourcc = cv.VideoWriter_fourcc(*'MP4V')
-videoWriter = cv.VideoWriter('screen_recording_{}.mp4'.format(gettime), fourcc, 20.0, (WIDTH,  HEIGHT))
+class VideoRecorder(object):
+    def __init__(self):
+        pass
+
+    def start(self):
+        fourcc = cv.VideoWriter_fourcc(*'MP4V')
+        gettime = time.strftime("%Y%m%d_%H%M%S")
+        self.name = 'RECORD_{}.mp4'.format(gettime)
+        # Recorder-1: Define the codec and create VideoWriter object
+        self.writer = cv.VideoWriter(self.name, fourcc, 20.0, (WIDTH,  HEIGHT))
+
+    def write(self, img):
+        self.writer.write(img)
+    
+    def release(self):
+        self.writer.release()
 
 def textInfo(txt, imgTarget, xpos, ypos):
     ''' Put text infomation on an image '''
@@ -30,20 +38,26 @@ def textInfo(txt, imgTarget, xpos, ypos):
 
 def grabScreen():
     ''' The Main Function! '''
+    vrecorder = VideoRecorder()
 
     imgSet = None # x image
     img = None    # y image
     alarm = 0     # 动态定时器
     nonzero = 0
     record_switch = False
-
     fastMode = False
+
+    btmInfo = "Initializ...."
+    btmLife = 4.5
+    tmset = time.time()
+    tmnow = time.time()
 
     while True:
         imgSet = img  # y --> x
-        im = np.array(sct.grab(monitor)) # BGRA 图片
-        img = cv.cvtColor(im, cv.COLOR_BGRA2BGR)  # 格式转化
+        grabImgTmp = np.array(sct.grab(monitor)) # BGRA 图片
+        img = cv.cvtColor(grabImgTmp, cv.COLOR_BGRA2BGR)  # 格式转化
 
+        # 动态触发逻辑  nonzero 的数值触发 alarm
         if imgSet is not None:  # x image 获得了图像
             subimg = cv.subtract(img, imgSet)  # 相减
             gray = cv.cvtColor(subimg, cv.COLOR_BGR2GRAY) # 灰度
@@ -59,17 +73,32 @@ def grabScreen():
 
         # 录像写入条件： 触发状态 (alarm > 0)
         if record_switch and alarm > 0:
-            videoWriter.write(img)
+            vrecorder.write(img)
 
         # 显示缩小图和信息
         showImg = img.copy()
         resizedImg = cv.resize(showImg, (480, 270))
-        textInfo("FastMode: {}, NonZero: {}, Alarm Level: {}, Recording: {}".format(fastMode, nonzero, alarm, record_switch), resizedImg, 10, 20)
+        textInfo("FastMode: {}, Recording: {}".format(fastMode, record_switch), resizedImg, 10, 20)
+        textInfo("Alarm Level: {}".format(alarm), resizedImg, 10, 40)
+        textInfo("NonZero: {}".format(nonzero), resizedImg, 10, 60)
         if record_switch:
             if alarm > 0:  # RED sign
-                cv.circle(resizedImg,(20,60), 15, (0,0,255), -1)
+                cv.circle(resizedImg,(20,80), 12, (0,0,255), -1)
             else:          # Yellow sign
-                cv.circle(resizedImg,(20, 60), 15, (0,255,255), -1)
+                cv.circle(resizedImg,(20, 80), 12, (0,255,255), -1)
+            if fastMode:
+                textInfo("F".format(nonzero, alarm), resizedImg, 16, 86)
+        # bottom information
+        tmset = tmnow
+        tmnow = time.time()
+        interv = tmnow - tmset
+
+        btmLife -= interv
+        if btmLife < 0:
+            btmLife = 0
+        else:
+            textInfo(btmInfo, resizedImg, 20, 250)
+
         cv.imshow("Video Stream Monitor", resizedImg)
         #cv.moveWindow("Video Stream Monitor", 1350, 750)
 
@@ -79,15 +108,30 @@ def grabScreen():
             nonzero = 0
         else:
             key = cv.waitKey(29)
-        if key == ord('r'):
+
+        if key == ord('p'): # Recording / Pause Toggle
             record_switch = not record_switch
-        elif key == ord('f'):
+
+        elif key == ord('f'): # Fast mode Toggle
             fastMode = not fastMode
+
+        elif key == ord('s'): # Start
+            vrecorder.start()
+            record_switch = True
+            btmLife = 4.5
+            btmInfo = "Start Recording!"
+            print("===> Start Recording!")
+
+        elif key == ord('x'): # Stop
+            vrecorder.release()
+            btmLife = 4.5
+            btmInfo = "Recording Stopped, video file saved!"
+            print("===> saved...\n")
+
         elif key & 0xff == 27:
             break
         
-    # 结束， 释放录像, 关闭所有窗口
-    videoWriter.release()
+    #  关闭所有窗口
     cv.destroyAllWindows()
 
 if __name__ == "__main__":
