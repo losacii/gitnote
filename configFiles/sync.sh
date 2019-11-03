aa758ec3e1886af89fcfb5454e9666103f49e395
losacii <losacii5@gmail.com>
2019-11-03 05:27:45 -0500

    "update"

--- configFiles/sync.sh
+++ configFiles/sync.sh
@@ -1,38 +1,41 @@
 
 echo "Deleting..."
-rm  ~/.bash_aliases              
-rm  ~/.bashrc                    
-rm  ~/.zshrc                     
-rm  ~/.inputrc                   
-rm  ~/.config/i3/config          
-rm  ~/.i3status.conf             
-rm  ~/.config/kitty/kitty.conf   
-rm  ~/.config/vifm/vifmrc        
-rm  ~/.xinitrc                   
-rm  ~/.Spacevim.dBack/init.toml
-rm  ~/.SpacevimBack/config/main.vim
+rm ~/.bash_aliases              
+rm ~/.bashrc                    
+rm ~/.inputrc                   
+rm ~/.config/nvim/init.vim
+#rm  ~/.zshrc                     
+#rm  ~/.config/i3/config          
+#rm  ~/.i3status.conf             
+#rm  ~/.config/kitty/kitty.conf   
+#rm  ~/.config/vifm/vifmrc        
+#rm  ~/.xinitrc                   
+#rm  ~/.Spacevim.dBack/init.toml
+#rm  ~/.SpacevimBack/config/main.vim
 
 sudo rm /etc/default/keyboard
 
 
 echo "linking..."
-sudo ln confiles/_etc.default.keyboard /etc/default/keyboard
 
 ln  confiles/_bash_aliases      ~/.bash_aliases              
 ln  confiles/_bashrc            ~/.bashrc                    
-ln  confiles/_zshrc             ~/.zshrc                     
 ln  confiles/_inputrc           ~/.inputrc                   
+ln  confiles/_vimrc             ~/.config/nvim/init.vim
 
-ln  confiles/_i3config          ~/.config/i3/config          
-ln  confiles/_i3status.conf     ~/.i3status.conf             
-ln  confiles/_kittyConfig       ~/.config/kitty/kitty.conf   
+#ln  confiles/_zshrc             ~/.zshrc                     
 
-ln  confiles/_vifmrc            ~/.config/vifm/vifmrc        
-ln  confiles/_xinitrc           ~/.xinitrc                   
+#ln  confiles/_i3config          ~/.config/i3/config          
+#ln  confiles/_i3status.conf     ~/.i3status.conf             
+#ln  confiles/_kittyConfig       ~/.config/kitty/kitty.conf   
 
-ln  confiles/_spaceFVD.vim      ~/.Spacevim.dBack/init.toml
-ln  confiles/_spaceMain.vim     ~/.SpacevimBack/config/main.vim
+#ln  confiles/_vifmrc            ~/.config/vifm/vifmrc        
+#ln  confiles/_xinitrc           ~/.xinitrc                   
 
+#ln  confiles/_spaceFVD.vim      ~/.Spacevim.dBack/init.toml
+#ln  confiles/_spaceMain.vim     ~/.SpacevimBack/config/main.vim
+
+#sudo ln confiles/_etc.default.keyboard /etc/default/keyboard
 echo "Done!"
 
 
