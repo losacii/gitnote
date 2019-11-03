e6147274c30ca6d8b21b104420710e91c556568f
apple <apple@appledeMac-mini.local>
2019-11-03 09:00:04 +0800

    "update"

--- cv2_grabScreen/compressVideo.py
+++ cv2_grabScreen/compressVideo.py
@@ -4,7 +4,10 @@ files = os.listdir('.')
 for f in files:
     if f.endswith('.mp4'):
         print("\n--------> Compressing", f)
-        os.system("ffmpeg -i {} -vcodec libx265 -crf 24 {}".format(f, '_'+f))
+        #os.system("ffmpeg -i {} -vcodec libx265 -crf 24 {}".format(f, '_'+f))
+        os.system("ffmpeg -i {} -b 255000 {}".format(f, '_'+f))
+        os.system("rm {}".format(f))
+        
 
 # ffmpeg -i input.mp4 -vcodec libx265 -crf 24 output.mp4
 
