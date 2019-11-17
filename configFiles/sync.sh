c3c47750cd0f88560fc16e3f1f2686afb9d3ad18
losacii <losacii5@gmail.com>
2019-11-17 09:53:04 -0500

    "update"

--- configFiles/sync.sh
+++ configFiles/sync.sh
@@ -4,12 +4,12 @@ rm ~/.bash_aliases
 rm ~/.bashrc                    
 rm ~/.inputrc                   
 rm ~/.config/nvim/init.vim
-#rm  ~/.zshrc                     
-#rm  ~/.config/i3/config          
-#rm  ~/.i3status.conf             
-#rm  ~/.config/kitty/kitty.conf   
-#rm  ~/.config/vifm/vifmrc        
-#rm  ~/.xinitrc                   
+rm  ~/.zshrc                     
+rm  ~/.config/i3/config          
+rm  ~/.i3status.conf             
+rm  ~/.config/kitty/kitty.conf   
+rm  ~/.config/vifm/vifmrc        
+rm  ~/.xinitrc                   
 #rm  ~/.Spacevim.dBack/init.toml
 #rm  ~/.SpacevimBack/config/main.vim
 
@@ -18,17 +18,17 @@ sudo rm /etc/default/keyboard
 echo "linking..."
 
 ln  confiles/_bash_aliases      ~/.bash_aliases              
-ln  confiles/_bashrc            ~/.bashrc                    
+cp  confiles/_bashrc            ~/.bashrc                    
 ln  confiles/_inputrc           ~/.inputrc                   
 ln  confiles/_vimrc             ~/.config/nvim/init.vim
 
 #ln  confiles/_zshrc             ~/.zshrc                     
 
-#ln  confiles/_i3config          ~/.config/i3/config          
-#ln  confiles/_i3status.conf     ~/.i3status.conf             
-#ln  confiles/_kittyConfig       ~/.config/kitty/kitty.conf   
+cp  confiles/_i3config          ~/.config/i3/config          
+cp  confiles/_i3status.conf     ~/.i3status.conf             
+cp  confiles/_kittyConfig       ~/.config/kitty/kitty.conf   
 
-#ln  confiles/_vifmrc            ~/.config/vifm/vifmrc        
+ln  confiles/_vifmrc            ~/.config/vifm/vifmrc        
 #ln  confiles/_xinitrc           ~/.xinitrc                   
 
 #ln  confiles/_spaceFVD.vim      ~/.Spacevim.dBack/init.toml
