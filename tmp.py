72cacbbdbe50f255deee91558c48840c9c57868a
losacii <losacii5@gmail.com>
2019-11-24 11:31:26 -0500

    update

--- tmp.py
+++ tmp.py
@@ -1,3 +1,4 @@
+
 import os
 import sys
 import time
@@ -14,3 +15,35 @@ print('\n' * 16)
 
 
 
+
+'''
+
+snippet t "A simple HTML tag" b
+<${1:div}>
+    hello world
+</${1/(\w+).*/$1/}>
+endsnippet
+
+snippet todo "TODO reminder" b
+<!-- TODO: `echo $USER` ${1:description} -->
+endsnippet
+
+snippet vstm "Vimscript time"
+`!v strftime("%c")`
+endsnippet
+	
+
+snippet 'rep (\d+) (\S+)' "repeat word n times" r
+`!p 
+count = int(match.group(1))
+snip.rv = count * (match.group(2) + ' ')`
+endsnippet
+
+snippet 'repn (\d+) (.+)' "repeat word n times" r
+`!p 
+count = int(match.group(1))
+word = match.group(2)
+snip.rv = count * (word + '\n')`
+endsnippet
+
+'''
