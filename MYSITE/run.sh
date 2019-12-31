24b02bc1dd527966d4b5fca8b6e7a58ec70ec6d4
losacii <losacii5@gmail.com>
2019-12-31 06:01:46 -0500

    "update"

--- MYSITE/run.sh
+++ MYSITE/run.sh
@@ -1,9 +1,17 @@
-if [ $1 -eq 0  ]
+# run in background with this command
+# nohup python manage.py runserver 0.0.0.0:8999 &
+
+echo "$1 is 5? " 
+if (( $1 == 5 ))
 then
-    python manage.py runserver 0.0.0.0:8000
-elif [ $1 -eq 1  ]
-then
-    nohup python manage.py runserver 0.0.0.0:8999 &
+    echo "===> Otion $1: starting server"
+    python manage.py runserver 0.0.0.0:8999
 else
-    echo "Something Else"
+    echo "===> Otion $1: starting server"
+    nohup python manage.py runserver 0.0.0.0:8999 &
+fi
+
+if (( $1 == 'b' ))
+then
+    echo "background"
 fi
