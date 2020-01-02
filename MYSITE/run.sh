7939dc73dff1f7ef7b376c770e6fa18d7177a603
losacii <losacii5@gmail.com>
2020-01-02 03:20:45 -0500

    "update"

--- MYSITE/run.sh
+++ MYSITE/run.sh
@@ -1,17 +1,15 @@
 # run in background with this command
 # nohup python manage.py runserver 0.0.0.0:8999 &
 
-echo "$1 is 5? " 
-if (( $1 == 5 ))
+echo "$1" 
+if (( $1 == 0 ))
 then
     echo "===> Otion $1: starting server"
     python manage.py runserver 0.0.0.0:8999
-else
-    echo "===> Otion $1: starting server"
-    nohup python manage.py runserver 0.0.0.0:8999 &
-fi
-
-if (( $1 == 'b' ))
+elif (( $1 == 1 ))
 then
-    echo "background"
+    echo "===> Otion $1: starting server, on background..."
+    nohup python manage.py runserver 0.0.0.0:8999 &
+else
+    echo "===> something else..."
 fi
